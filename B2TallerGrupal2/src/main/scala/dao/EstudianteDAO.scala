f02a0388877fda35283744cc6ef16e4a20643b0e
NicolayJ <dnjimenez6@utpl.edu.ec>
2025-01-24 08:11:47 -0500

    trabajo

---
+++ B2TallerGrupal2/src/main/scala/dao/EstudianteDAO.scala
@@ -0,0 +1,29 @@
+package dao
+
+import doobie._
+import doobie.implicits._
+import cats.effect.IO
+import cats.implicits._
+
+import models.Estudiante
+import config.Database
+
+object EstudianteDAO {
+  def insert(estudiante: Estudiante): ConnectionIO[Int] = {
+    sql"""
+     INSERT INTO estudiantes (nombre, edad, calificacion, genero)
+     VALUES (
+       ${estudiante.nombre},
+       ${estudiante.edad},
+       ${estudiante.calificacion},
+       ${estudiante.genero}
+     )
+   """.update.run
+  }
+
+  def insertAll(estudiantes: List[Estudiante]): IO[List[Int]] = {
+    Database.transactor.use { xa =>
+      estudiantes.traverse(t => insert(t).transact(xa))
+    }
+  }
+}
