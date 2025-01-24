29e1a30d6c6a17d170f1368c225b8a286c0e7e72
NicolayJ <dnjimenez6@utpl.edu.ec>
2025-01-24 08:18:37 -0500

    Lectura

--- B2TallerGrupal2/src/main/scala/Main.scala
+++ B2TallerGrupal2/src/main/scala/Main.scala
@@ -3,15 +3,15 @@ import kantan.csv._
 import kantan.csv.ops._
 import kantan.csv.generic._
 import java.io.File
-import models.Temperatura
-import dao.TemperaturaDAO
+import models.Estudiante
+import dao.EstudianteDAO
 
 // Extiende de IOApp.Simple para manejar efectos IO y recursos de forma segura
 object Main extends IOApp.Simple {
   val path2DataFile2 = "src/main/resources/data/estudiantes.csv"
 
   val dataSource = new File(path2DataFile2)
-    .readCsv[List, Temperatura](rfc.withHeader.withCellSeparator(','))
+    .readCsv[List, Estudiante](rfc.withHeader.withCellSeparator(','))
 
   val estudiantes = dataSource.collect {
     case Right(estudiante) => estudiante
@@ -19,7 +19,7 @@ object Main extends IOApp.Simple {
 
   // Secuencia de operaciones IO usando for-comprehension
   def run: IO[Unit] = for {
-    result <- TemperaturaDAO.insertAll(estudiantes)  // Inserta datos y extrae resultado con <-
+    result <- EstudianteDAO.insertAll(estudiantes)  // Inserta datos y extrae resultado con <-
     _ <- IO.println(s"Registros insertados: ${result.size}")  // Imprime cantidad
   } yield ()  // Completa la operación
 }
